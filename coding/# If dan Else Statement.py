0393b36ed02b313bb418d62bbaef3fe60f31a591
IrsyadMuahamad <irsyadmuhamad@gmail.com>
2022-04-22 20:28:18 +0700

    UTS_KB

---
+++ coding/# If dan Else Statement.py
@@ -0,0 +1,8 @@
+nama = input("apakah ada makanan yang bisa di berikan? ")
+
+if nama=="ada":
+	print("ada, di atas meja!!!")
+else:
+	print("tidak ada, beli di supermarket dulu! :(")
+
+print("akhir dari percakapan.")
---
+++ coding/UTS_ifelse.py
@@ -0,0 +1,14 @@
+# ELIF = else if statement
+
+nama = input("Nama anda siapa? ")
+
+if nama == "saya iqbal": # kondisi 1
+	print("Hai, anak bola!") # aksi true 1
+elif nama == "saya mario": # kondisi 2
+	print("Hai si anak gamer!!") # aksi true 2
+elif nama == "mario": # kondisi 3
+	print("Hai si humoris!") # aksi true 3
+else:
+	print("kamu siapa, saya tidak kenal!!!") # aksi false
+
+print("ini adalah akhir dari percakapan")
